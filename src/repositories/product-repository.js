2d669f6275c0601f8c0afb425d40c1c369702a9b
vitormoschetta <vitormoschetta@gmail.com>
2020-12-26 20:39:49 -0400

    update for async await

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,41 +3,59 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
-exports.getAll = () => {
-    return Product.find({ },' _id name price')   
+
+exports.getAll = async () => {
+    const res = await Product.find({}, '_id name price')
+    return res
     // o primeiro parâmetro de find() são os filtros, caso existam   
     // o segundo parâmetro de find() são as propriedades que desejamos retornar                                 
 }
 
-exports.getById = (id) => {
-    return Product.findById({ _id: id }, '_id name price')      
+
+exports.getById = async (id) => {
+    const res = await Product.findById({ _id: id }, '_id name price')
+    return res
 }
 
-exports.create = (data) => {     
-    let product = new Product()
-    product.name = data.name
-    product.price = data.price    
-    return product.save()  
+
+exports.create = async (data) => {
+    let product = new Product(data)
+    await product.save()
 }
 
-exports.update = (id, data) => {
-    return Product
+
+exports.update = async (id, data) => {
+    await Product
         .findByIdAndUpdate(id, {
-           $set: {
-               name: data.name,
-               price: data.price
-           } 
-        })      
+            $set: {
+                name: data.name,
+                price: data.price
+            }
+        })
 }
 
-exports.delete = (id) => {
-    return Product.findByIdAndRemove({_id: id})       
+
+exports.delete = async (id) => {
+    await Product.findByIdAndRemove({ _id: id })
 }
 
-function Exists(name) {
-    let product = Product.find({ name: name },' _id name price')  
+
+exports.exists = async (name) => {
+    let product = await Product.findOne({ name: name })
     if (product != null)
         return true
+
+    return false
+}
+
+
+exports.existsUpdate = async (id, name) => {
+    let product = await Product.findOne({ name: name })
+
+    if (product != null) {
+        if (product.id != id)
+            return true
+    }
 
     return false
 }
